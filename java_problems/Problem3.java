576e5c60608b88e164197410bc5305dc449702d7
TakumiHashimotoFu <t.hashimoto.fu2021@gmail.com>
2023-03-17 00:29:33 +0900

    問題追加

---
+++ java_problems/Problem3.java
@@ -0,0 +1,33 @@
+public class Problem3 {
+    public static void main(String[] args) {
+    }
+}
+/*
+課題3
+1から100までの整数に対し、
+- 3で割り切れる場合は「Fizz」
+- 5で割り切れる場合は「Buzz」
+- 3でも5でも割り切れる場合は「FizzBuzz」
+- 上記以外は数値そのまま
+を表示してください。
+【実行結果】
+例)1から17までの表示結果
+----------
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+17
+*/
